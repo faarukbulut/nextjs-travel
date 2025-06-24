38a1329e484487426ef871729b0c68a5143e96e6
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2025-06-24 22:41:18 +0300

    contact sayfası & zod validation

--- travel/src/app/(routes)/contact/page.tsx
+++ travel/src/app/(routes)/contact/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ContactForm from './_components/contact-form'
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div>
+      <ContactForm />
+    </div>
   )
 }
 
